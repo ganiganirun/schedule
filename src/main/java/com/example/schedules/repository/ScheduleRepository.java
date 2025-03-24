426633c762a0fe4cb1ab7ff665d1d76464f2b69c
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 12:06:36 +0900

    feat: ScheduleRepository 인터페이스 일정 저장 기능

---
+++ src/main/java/com/example/schedules/repository/ScheduleRepository.java
@@ -0,0 +1,12 @@
+package com.example.schedules.repository;
+
+import com.example.schedules.dto.ScheduleResponseDto;
+import com.example.schedules.entity.Schedule;
+import java.util.List;
+
+public interface ScheduleRepository {
+  ScheduleResponseDto saveSchedule(Schedule schedule);
+
+
+
+}
