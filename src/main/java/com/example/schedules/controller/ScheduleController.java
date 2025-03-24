671fea724cff7642050dcc17feb0197a279ae63a
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 11:59:26 +0900

    feat: ScheduleController 클래스 포스트 기능 구현

---
+++ src/main/java/com/example/schedules/controller/ScheduleController.java
@@ -0,0 +1,37 @@
+package com.example.schedules.controller;
+
+
+import com.example.schedules.dto.ScheduleRequestDto;
+import com.example.schedules.dto.ScheduleResponseDto;
+import com.example.schedules.service.ScheduleService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedules")
+public class ScheduleController {
+
+  private final ScheduleService scheduleService;
+
+  public ScheduleController(ScheduleService scheduleService) {
+    this.scheduleService = scheduleService;
+  }
+
+
+  @PostMapping
+  public ResponseEntity<ScheduleResponseDto> createSchedule(@RequestBody ScheduleRequestDto dto){
+
+    return new ResponseEntity<>(scheduleService.saveSchedule(dto), HttpStatus.OK);
+
+  }
+
+
+
+}
