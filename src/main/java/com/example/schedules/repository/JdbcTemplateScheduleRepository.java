579a67b9bf0efeff1b1322b80f606b58dfa6ccc1
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 15:50:16 +0900

    feat: JdbcTemlatedScheduleRepository 클래스 일정 update 오버라이딩
    내림차순 조회 기능 추가

--- src/main/java/com/example/schedules/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/example/schedules/repository/JdbcTemplateScheduleRepository.java
@@ -46,7 +46,7 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository{
   @Override
   public List<ScheduleResponseDto> findAllSchedules(String update_at, String name) {
 
-    return jdbcTemplate.query("select * from schedule where name = ? and left(updated_at, 10) >= ? order by str_to_date(updated_at, '%Y-%m-%d %H:%i:%s')desc", ScheduleRowMapper(), name, update_at);
+    return jdbcTemplate.query("select * from schedule where name = ? and left(updated_at, 10) >= ? order by updated_at desc", ScheduleRowMapper(), name, update_at);
   }
 
   @Override
