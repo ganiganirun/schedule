af32b8ab4e0d5808fa40e6f354ca916411936505
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 12:01:17 +0900

    feat: ScheduleRequestDto 클래스 생성

---
+++ src/main/java/com/example/schedules/dto/ScheduleRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.schedules.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+
+  private String name;
+  private String todo;
+  private String password;
+
+}
