82dfd274aaf7f1788be86c237363ca646b312c87
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 12:01:55 +0900

    feat: ScheduleResponseDto 클래스 정의

---
+++ src/main/java/com/example/schedules/dto/ScheduleResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.schedules.dto;
+
+import com.example.schedules.entity.Schedule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleResponseDto {
+
+  private Long id;
+  private String name;
+  private String todo;
+  private String updatedAt;
+
+  public ScheduleResponseDto(Schedule schedule){
+    this.id = schedule.getId();
+    this.name = schedule.getName();
+    this.todo = schedule.getTodo();
+    this.updatedAt = schedule.getUpdatedAt();
+  }
+
+
+}
