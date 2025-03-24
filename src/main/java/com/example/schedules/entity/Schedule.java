a149601369dd443c1ae54ddf2da9d1abd08cb50f
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-24 12:02:49 +0900

    feat: Schedule 클래스 생성
    속성 및 생성자 선언 및 초기화

---
+++ src/main/java/com/example/schedules/entity/Schedule.java
@@ -0,0 +1,31 @@
+package com.example.schedules.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Schedule {
+
+  private Long id;
+  private String name;
+  private String todo;
+  private String createdAt;
+  private String updatedAt;
+  private String password;
+
+  public Schedule(String name, String todo, String password, String createdAt, String updatedAt){
+    this.name = name;
+    this.todo = todo;
+    this.password = password;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Schedule(String name, String todo, String password, String updatedAt){
+    this.name = name;
+    this.todo = todo;
+    this.password = password;
+    this.updatedAt = updatedAt;
+  }
+}
